a9e7f7c63c0a94fc54fe7b6863328fe74f259b0f
lukechadwick <llukechadwick@gmail.com>
2018-08-30 20:08:07 +1200

    decals

---
+++ index.js
@@ -0,0 +1,36 @@
+var decalImage = new Image;
+
+
+decalImage.src = './public/smash.png';
+
+let weaponImage = './public/wGun.png'
+
+
+document.addEventListener('click', function(e) {
+    e = e || window.event;
+    var decalImg = document.createElement('img');
+
+    let offset = 120;
+
+    decalImg.src = decalImage.src;
+    decalImg.style.position = 'absolute';
+    decalImg.style.height = '150px';
+    decalImg.style.width = '150px';
+    decalImg.style.top = (e.pageY - offset || e.clientY - offset) + 'px';
+    decalImg.style.left = (e.pageX - offset || e.clientX- offset) + 'px';
+    this.body.appendChild(decalImg);
+}, false);
+
+
+document.addEventListener('mousemove', function(e) {
+    e = e || window.event;
+    var weaponImg = document.getElementById('curWeapon');
+
+    let offset = 120;
+
+    weaponImg.src = weaponImage
+    weaponImg.style.position = 'absolute';
+    weaponImg.style.top = (e.pageY - offset || e.clientY - offset) + 'px';
+    weaponImg.style.left = (e.pageX - offset || e.clientX- offset) + 'px';
+
+}, false);
