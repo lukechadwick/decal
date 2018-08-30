92b0a681f841e9cfa9a74b1b57f8d44472c4d829
harrison@devacademy.co.nz <harrison-symes>
2018-08-31 09:11:39 +1200

    finish assets2

--- index.js
+++ index.js
@@ -1,25 +1,9 @@
 var decalImage = new Image;
 
 
-decalImage.src = './public/bullethole.png';
+decalImage.src = './public/blood.png';
 
-let weaponImage = './public/wGun.png'
-
-
-document.addEventListener('click', function(e) {
-    e = e || window.event;
-    var decalImg = document.createElement('img');
-
-    let offset = 120;
-
-    decalImg.src = decalImage.src;
-    decalImg.style.position = 'absolute';
-    decalImg.style.height = '150px';
-    decalImg.style.width = '150px';
-    decalImg.style.top = (e.pageY - offset || e.clientY - offset) + 'px';
-    decalImg.style.left = (e.pageX - offset || e.clientX- offset) + 'px';
-    this.body.appendChild(decalImg);
-}, false);
+let weaponImage = './public/wKnife.png'
 
 
 document.addEventListener('mousemove', function(e) {
@@ -30,7 +14,27 @@ document.addEventListener('mousemove', function(e) {
 
     weaponImg.src = weaponImage
     weaponImg.style.position = 'absolute';
+    weaponImg.style.zIndex = 1;
     weaponImg.style.top = (e.pageY - offset || e.clientY - offset) + 'px';
     weaponImg.style.left = (e.pageX - offset || e.clientX- offset) + 'px';
 
 }, false);
+
+
+document.addEventListener('click', function(e) {
+    e = e || window.event;
+    var decalImg = document.createElement('img');
+
+    let offset = 120;
+
+    decalImg.src = decalImage.src;
+    decalImg.style.zIndex = 0;
+
+    decalImg.style.position = 'absolute';
+    decalImg.style.height = '150px';
+    decalImg.style.width = '150px';
+    decalImg.style.top = (e.pageY - offset || e.clientY - offset) + 'px';
+    decalImg.style.left = (e.pageX - offset || e.clientX- offset) + 'px';
+    this.body.appendChild(decalImg);
+}, false);
+
